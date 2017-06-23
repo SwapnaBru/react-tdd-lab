f0e8436079ba38605b832ea8f16eda46202e40e3
Adam Fabicki <adam@flashstock.com>
2017-06-23 00:03:13 -0400

    phone number validator handles undefined input

--- src/validators/phoneNumber.js
+++ src/validators/phoneNumber.js
@@ -1,3 +1,3 @@
 export default ((value) => {
-    return value.length === 0 || (value.match(/^[\d \-]+$/) && value.replace(/\D/g, '').length === 10) ? undefined : 'should be a valid phone number'
+    return value && !(value.length === 0 || (value.match(/^[\d \-]+$/) && value.replace(/\D/g, '').length === 10)) ? 'should be a valid phone number': undefined
 })
